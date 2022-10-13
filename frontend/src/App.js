153eea48029640468e47775bc4b88ed5915fe705
Ketan <ketansali7600@gmail.com>
2022-10-13 09:36:03 +0530

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './components/Header';
+import SuperAdmin from './components/sidebar/SuperAdmin';
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <SuperAdmin/>
+    
+    </>
+  );
+}
+
+export default App;
